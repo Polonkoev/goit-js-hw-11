23ebee9ef107dc420b1066e56b43e184daf5ed11
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-10-21 23:54:42 +0600

    query params fixed

--- src/script.js
+++ src/script.js
@@ -3,55 +3,79 @@ import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const inputForm = document.getElementById('search-form')
-const inputValue = document.querySelector('.input')
+const inputForm = document.getElementById('search-form');
+const inputValue = document.querySelector('.input');
+const container = document.querySelector('.gallery')
 const loadMore = document.querySelector('.load-more');
 
-loadMore.style.display = 'flex'
+// loadMore.style.display = 'flex';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '30755005-c126f789c706217abec8a0f9e';
 let inputEl = '';
-let page = 1
+let page = 1;
 
-inputForm.addEventListener('submit', ((event) => {
-    event.preventDefault();
-    inputEl = inputValue.value
-    query()
-console.log(inputValue.value);
-inputForm.reset()
-}))
 
-const searchParams = new URLSearchParams({
+let queryParams = {
   key: API_KEY,
-  q: `${inputEl}`,
+  q: inputValue.value,
   image_type: 'photo',
   orientation: 'horizontal',
   safesearch: true,
   page: page,
   per_page: 40,
+};
+
+
+inputForm.addEventListener('submit', event => {
+  event.preventDefault();
+  
+
+  queryParams.q = inputValue.value;
+
+  
+
+  query()
+  .then(response => {
+    if (response.status !== 200) {
+      throw new Error();
+    }
+    return response.data;
+  })
+  .then(data => {
+console.log(data);
+    data.hits.map((item) => {
+      container.insertAdjacentHTML('afterbegin', `<li><img src="${item.previewURL}" alt="gg"></li>`)
+    })
+    console.log(data);
+    // Data handling
+  })
+  .catch(error => {
+    console.error(error)
+  });
+  inputForm.reset();
 });
-
-
-
-console.log(searchParams.toString()); 
 
 async function query() {
   try {
+    const searchParams = new URLSearchParams(queryParams)
     const response = await axios.get(`${BASE_URL}?${searchParams}`);
-    const todoItems = response.data;
-    console.log(`GET:from Pixabay`, todoItems);
-    return todoItems;
+    const todoItems = response;
+    console.log(response.status);
+    console.log(searchParams.toString());
+    console.log(`RESPONSE:from Pixabay`, todoItems);
+    return   todoItems;
   } catch (errors) {
     console.error(errors);
   }
 }
-// // query();
 
-loadMore.addEventListener('click', ()=>{
-page += 1
 
-    console.log(inputValue);
-    console.log(page);
-    console.log(searchParams.toString());  
-});
+
+// loadMore.addEventListener('click', ()=>{
+// page += 1
+
+//     console.log(inputValue);
+//     console.log(page);
+//     console.log(searchParams.toString());
+// });
